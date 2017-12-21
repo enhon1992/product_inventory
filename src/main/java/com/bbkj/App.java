4fc6761542069ee74b7f8f9f737520abdfd3ded8
Enhon <yinhao@bb-kj.com>
2017-12-21 11:58:56 +0800

    去除@EnableTransactionManagement

--- src/main/java/com/bbkj/App.java
+++ src/main/java/com/bbkj/App.java
@@ -10,6 +10,7 @@ import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class App {
     public static void main(String[] args) throws SQLException {
         ConfigurableApplicationContext context = SpringApplication.run(App.class,args);
