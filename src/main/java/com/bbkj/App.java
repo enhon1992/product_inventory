6ebfa230812c5a3b34879f3a960784bef8861b2f
Enhon <yinhao@bb-kj.com>
2017-12-21 14:18:30 +0800

    去除冗余代码

--- src/main/java/com/bbkj/App.java
+++ src/main/java/com/bbkj/App.java
@@ -1,21 +1,12 @@
 package com.bbkj;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.SpringBootApplication;;
 import java.sql.SQLException;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) throws SQLException {
-        ConfigurableApplicationContext context = SpringApplication.run(App.class,args);
-        DruidDataSource dataSource= (DruidDataSource) context.getBean(DataSource.class);
-//        dataSource.init();//初始化DruidDataSource
-//        SqlSessionFactory sqlSessionFactory = context.getBean(SqlSessionFactory.class);
-//        System.out.println(context.getBean(DataSourceTransactionManager.class));
+        SpringApplication.run(App.class,args);
     }
 }
