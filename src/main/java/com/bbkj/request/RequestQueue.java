dd75e8c7ffdbf4562aceb8bf05c3a6af4fa91c7e
Enhon <yinhao@bb-kj.com>
2017-12-25 18:23:45 +0800

    flagMap

--- src/main/java/com/bbkj/request/RequestQueue.java
+++ src/main/java/com/bbkj/request/RequestQueue.java
@@ -2,7 +2,9 @@ package com.bbkj.request;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 请求内存队列
@@ -15,6 +17,14 @@ public class RequestQueue {
 	 * 内存队列
 	 */
 	private List<ArrayBlockingQueue<InventoryRequest>> queues = new ArrayList<ArrayBlockingQueue<InventoryRequest>>();
+
+	/**
+	 * 标识位map  可以就是商品的id 因为是在高并发的场景下所以这里要使用线程安全的Map  ConcurrentHashMap
+	 *flagMap只要是用来优化请求去重的  表面一看去重应该是具体的某个内存队列对路由的自己某个具体的商品的请求去重
+	 *所以这个flagMap不应该定义在这里，但是实际上 这样定义是没有错。愿意就是因为个商品的请求肯定都会被路由的同一个内存队列中
+	 * 而这里的flagMap的key就是商品的id，那么同一个key肯定对应内存队列也是确定的
+	 */
+	private Map<Integer, Boolean> flagMap = new ConcurrentHashMap<Integer, Boolean>();
 
 	public ArrayBlockingQueue<InventoryRequest> getQueue(int index) {
 		return this.queues.get(index);
@@ -56,5 +66,9 @@ public class RequestQueue {
 	public int getQueueSize(){
 		return this.queues.size();
 	}
-	
+
+
+	public Map<Integer, Boolean> getFlagMap() {
+		return flagMap;
+	}
 }
